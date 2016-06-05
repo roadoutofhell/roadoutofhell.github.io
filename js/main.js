a50961698c10ebb133c9bd1ab975a4c07e3c9b63
Kozma Balazs <kozma.balazs1992@gmail.com>
2016-06-06 00:31:07 +0200

    archive hover, not click

--- js/main.js
+++ js/main.js
@@ -8,32 +8,12 @@ $('#slider2').hide();
 $('#slider3').hide();
 
 function workBelt(){
-    $('#slider1-wrap').click(function(){
+    $('#slider1-wrap').hover(function(){
         if ($('#slider1').is(':hidden')) {
             $('#slider1').slideDown(300)
             $('#slider1').css('top','100%');
         } else {
             $('#slider1').slideUp(300)
-        }
-    });
-
-    $('#slider2-wrap').click(function(){
-        if ($('#slider2').is(':hidden')) {
-            $('#slider2').show(300)
-            $('#slider2').css('top','100%');
-        } else {
-            $('#slider2').hide()
-            $('#slider2').css('top','-100%');
-        }
-    });
-
-    $('#slider3-wrap').click(function(){
-        if ($('#slider3').is(':hidden')) {
-            $('#slider3').show(300)
-            $('#slider3').css('top','100%');
-        } else {
-            $('#slider3').hide()
-            $('#slider3').css('top','-100%');
         }
     });
 }
