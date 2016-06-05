d678fd14d3006d060787900a3b4078955149fc4e
Kozma Balazs <kozma.balazs1992@gmail.com>
2016-06-05 21:07:38 +0200

    fully functional archive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(function(){
     workBelt();
+    workBelt2();
 });
 
 $('#slider1').hide();
@@ -9,11 +10,10 @@ $('#slider3').hide();
 function workBelt(){
     $('#slider1-wrap').click(function(){
         if ($('#slider1').is(':hidden')) {
-            $('#slider1').show(300)
+            $('#slider1').slideDown(300)
             $('#slider1').css('top','100%');
         } else {
-            $('#slider1').hide()
-            $('#slider1').css('top','-100%');
+            $('#slider1').slideUp(300)
         }
     });
 
@@ -35,5 +35,33 @@ function workBelt(){
             $('#slider3').hide()
             $('#slider3').css('top','-100%');
         }
+    });
+}
+
+function workBelt2(){
+    $('#2016majus').click(function(){
+        $('.work-belt2').css('left','-100%');
+        $('.work-wrap').show(400);
+        $('.main-wrapper').fadeOut(400);
+    });
+
+    $('#2016aprilis').click(function(){
+        $('.work-belt2').css('left','-100%');
+        $('.work-wrap2').show(400);
+        $('.main-wrapper').fadeOut(400);
+    });
+
+    $('#2016aprilis10').click(function(){
+        $('.work-belt2').css('left','-100%');
+        $('.work-wrap3').show(400);
+        $('.main-wrapper').fadeOut(400);
+    });
+
+    $('.work-return').click(function(){
+        $('.work-belt2').css('left','0%');
+        $('.main-wrapper').fadeIn(400);
+        $('.work-wrap').hide(400);
+        $('.work-wrap2').hide(400);
+        $('.work-wrap3').hide(400);
     });
 }
